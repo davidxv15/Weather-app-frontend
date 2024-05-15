45c762c818a0f4c39b2581b1b095ec4206505b62
davidxv15 <davidxvaz@gmail.com>
2024-05-14 22:41:01 -0700

    conversion functions

--- src/Weather.js
+++ src/Weather.js
@@ -18,17 +18,31 @@ const Weather = () => {
     fetchWeatherData();
   }, []);
 
+    // Function to convert temperature from Celsius to Fahrenheit
+    const convertToCelsius = (temperature) => {
+        return Math.round((temperature * 9/5) + 32);
+      };
+
+      // Function to convert wind speed from m/s to mph
+const convertToMPH = (speed) => {
+    return Math.round(speed * 2.23694); // 1 m/s = 2.23694 mph
+};
+const convertDewPoint = (celsius) => {
+    return Math.round((celsius * 9/5) + 32);
+  };
+
   return (
     <div>
       <h1>Weather App</h1>
       {weatherData ? (
         <div>
-          <h2>Temperature: {weatherData.temperature}</h2>
-          <h2>Feels Like: {weatherData.temperatureApparent}</h2>
+          <h2>Temperature: {convertToCelsius(weatherData.temperature)}°F</h2>
+          {/* <h2>Feels Like: {convertToCelsius(weatherData.temperatureApparent)}°F</h2> */}
           <h2>Humidity: {weatherData.humidity}%</h2>
-          <h2>Wind Speed: {weatherData.windSpeed}</h2>
-          <h2>Wind Direction: {weatherData.windDirection}</h2>
-          <h2>Dew Point: {weatherData.dewPoint}</h2>
+          <h2>Wind Speed: {convertToMPH(weatherData.windSpeed)} MPH</h2>
+          <h2>Wind Direction: {weatherData.windDirection}º</h2>
+          <h2>Dew Point: {convertDewPoint(weatherData.dewPoint)}ºF</h2>
+          {/* <h2>Visibility: {weatherData.visibility}</h2> */}
           {/* <h2>UV Index: {weatherData.uvIndex}</h2>
           <h2>Sunrise Time: {weatherData.sunriseTime}</h2>
           <h2>Sunset Time: {weatherData.sunsetTime}</h2> */}
