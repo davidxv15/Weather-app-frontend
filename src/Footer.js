b7b9aaba3100aa87128ae3a6b21a1b2888f46af0
davidxv15 <davidxvaz@gmail.com>
2024-05-20 15:58:24 -0700

    footer edit

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Data provided by <a href="tomorrow.io" target="_blank" rel="noopener noreferrer">Tomorrow.io</a></p>
+      <p>Powered by <a href="https://www.tomorrow.io/" target="_blank" rel="noopener noreferrer">Tomorrow.io</a></p>
     </footer>
   );
 };
