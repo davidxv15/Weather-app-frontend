9c70460a04eecc435f0a39760451aa073bdb20d4
davidxv15 <davidxvaz@gmail.com>
2024-05-22 22:53:01 -0700

    icon added

--- src/assets/components/WeatherIcon.js
+++ src/assets/components/WeatherIcon.js
@@ -4,13 +4,13 @@ import iconMapping from "../../weatherIconMapping.json";
 const WeatherIcon = ({ code }) => {
   const getIconUrl = (code) => {
     const iconName = iconMapping[code] || "assets/icons/default.svg"; // Fallback to default icon if none matched
-    return `${process.env.PUBLIC_URL}/${iconName}`;  // Adjust the path according to where your icons are stored
+    return `${process.env.PUBLIC_URL}/icons/${iconName}`;  // Adjust the path according to where your icons are stored
   };
 
   return (
     <img
       src={getIconUrl(code)}
-      alt="Current Weather Icon"
+      alt="Weather Icon"
       style={{ width: "100px", height: "100px" }}
     />
   );
