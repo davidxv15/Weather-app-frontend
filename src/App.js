b7087f5bde09b93108311b9f941e7f6fd518433b
davidxv15 <davidxvaz@gmail.com>
2024-06-18 00:26:03 -0700

    css min-height

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <div className="App">
       <Header />
       <Weather />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
--- src/assets/components/Header.js
+++ src/assets/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <h1>GetYerWeather</h1>
+            <h1>WeatherGetter</h1>
         </header>
     )
 };
