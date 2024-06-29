60a8565b0c41f8c7199425c5ced08851e805bbe8
davidxv15 <davidxvaz@gmail.com>
2024-06-29 04:38:37 -0700

    animation bg

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Powered by <a href="https://www.tomorrow.io/" target="_blank" rel="noopener noreferrer">Tomorrow.io</a> & <a href="https://sunrisesunset.io/" target="_blank">SunriseSunset.io</a></p>
+      <p className="powered">Powered by <a href="https://www.tomorrow.io/" target="_blank" rel="noopener noreferrer">Tomorrow.io</a> & <a href="https://sunrisesunset.io/" target="_blank">SunriseSunset.io</a></p>
     </footer>
   );
 };
