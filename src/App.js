c42a6ea1d996979d265e9820348b2beef6c1b53b
davidxv15 <davidxvaz@gmail.com>
2025-03-07 03:15:47 -0700

    SingingBowl.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import Header from "./assets/components/Header";
 import Footer from "./assets/components/Footer";
+import SingingBowl from "./SingingBowl";
 import "./App.css";
 
 const App = () => {
